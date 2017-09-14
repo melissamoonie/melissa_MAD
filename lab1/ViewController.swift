e7b515fd97448cfde7208524bbc0beb3434b9dde
melissamoonie <melissa.moonie@colorado.edu>
2017-09-14 15:49:57 -0600

    updated app

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func choosePic(_ sender: UIButton) {
+        if sender.tag == 1{
+            diveImage.image=UIImage(named: "mad3.png")
+        }
+        else if sender.tag == 2{
+            diveImage.image=UIImage(named: "mad2.png")
+        }
+    }
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var diveImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

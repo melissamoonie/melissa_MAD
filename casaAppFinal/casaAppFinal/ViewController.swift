b576d05362270fdba8786e56e1794832603cfe45
melissamoonie <melissa.moonie@colorado.edu>
2017-10-20 13:02:45 -0600

    Add files via upload

---
+++ casaAppFinal/casaAppFinal/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  casaAppFinal
+//
+//  Created by melissa on 10/20/17.
+//  Copyright © 2017 melissa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBAction func budget(_ sender: UISlider) {
+        let fontSize=sender.value;
+        fontSizeLabel.text=String(format: "%.0f", fontSize)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
